38866916296ce275e8a34890a6fe5b622dd3fdaa
hryushm <hryushm@gmail.com>
2019-04-04 01:08:19 +0900

    Add logging settings

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const notifySlack = require('./lib/notifySlack');
 const getAvailableDate = require('./lib/getAvailableDate');
 
 (async () => {
+  const date = new Date();
   const url = process.env.SLACK_URL;
   if (!url) {
     console.error('環境変数SLACK_URLがありません');
@@ -12,8 +13,11 @@ const getAvailableDate = require('./lib/getAvailableDate');
     const text = `利用可能な枠があります！\n${availableDate.join('\n')}`;
     try {
       await notifySlack(url, text);
+      console.log(date, text);
     } catch (error) {
       console.err(error);
     }
+  } else {
+    console.log(date, 'no available court');
   }
 })();
