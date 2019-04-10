62645d5f0f3942b15c165b2720e3d12c59d09d34
hryushm <hryushm@gmail.com>
2019-04-10 16:35:34 +0000

    Add state because it's better that bot notify only when available state is updated

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-const notifySlack = require('./lib/notifySlack');
-const getAvailableDate = require('./lib/getAvailableDate');
+const notifySlack = require("./lib/notifySlack");
+const getAvailableDate = require("./lib/getAvailableDate");
+const StateSaver = require("./lib/StateSaver");
 
-const reserveUrl = 'https://user.shinjuku-shisetsu-yoyaku.jp/regasu/reserve/gin_menu';
+const reserveUrl =
+  "https://user.shinjuku-shisetsu-yoyaku.jp/regasu/reserve/gin_menu";
 
 (async () => {
   const date = new Date();
   const url = process.env.SLACK_URL;
   if (!url) {
-    console.error('環境変数SLACK_URLがありません');
+    console.error("環境変数SLACK_URLがありません");
     process.exit(1);
   }
   try {
     const availableDate = await getAvailableDate();
-    if (availableDate.length > 0) {
-      const text = `利用可能な枠があります！\n${availableDate.join('\n')}\n予約はこちらから\n${reserveUrl}`;
+    const stateSaver = new StateSaver("./state.json");
+    const isSameState = stateSaver.isSameState(availableDate);
+    stateSaver.saveState(availableDate);
+    console.log(date, availableDate);
+    if (!isSameState) {
+      let text = "";
+      if (availableDate.length > 0) {
+        text = `利用可能な枠があります！\n${availableDate.join(
+          "\n"
+        )}\n予約はこちらから\n${reserveUrl}`;
+      } else {
+        text = "利用可能な枠がなくなりました。。";
+      }
       await notifySlack(url, text);
       console.log(date, text);
     } else {
-      console.log(date, 'no available court');
+      console.log(date, "no available court");
     }
   } catch (error) {
-    console.log('処理に失敗しました。。');
+    console.log("処理に失敗しました。。");
     console.log(error);
     process.exit(1);
   }
