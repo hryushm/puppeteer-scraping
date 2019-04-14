d7b8f3f04ea3b4983cf9b3f132950f8b6ce76558
hryushm <hryushm@gmail.com>
2019-04-14 14:25:11 +0900

    Refactoring

--- index.js
+++ index.js
@@ -1,45 +1,29 @@
 const notifySlack = require('./lib/notifySlack');
-const getAvailableDate = require('./lib/getAvailableDate');
+const TennisCourtScraper = require('./lib/getAvailableDate');
 const StateSaver = require('./lib/StateSaver');
 const logger = require('./lib/logger');
 
 const reserveUrl = 'https://user.shinjuku-shisetsu-yoyaku.jp/regasu/reserve/gin_menu';
 
 (async () => {
-  const date = new Date();
-  const url = process.env.SLACK_URL;
   let text = '';
   let shouldNotify = true;
-  if (!url) {
-    logger('環境変数SLACK_URLがありません');
-    process.exit(1);
-  }
   try {
-    const availableDate = await getAvailableDate();
+    const scraper = new TennisCourtScraper(reserveUrl);
     const stateSaver = new StateSaver('./state.json');
-    const isSameState = stateSaver.isSameState(availableDate);
-    stateSaver.saveState(availableDate);
-    if (!isSameState) {
-      if (availableDate.length > 0) {
-        text = `利用可能な枠があります！\n${availableDate.join(
-          '\n',
-        )}\n予約はこちらから\n${reserveUrl}`;
-      } else {
-        text = '利用可能な枠がなくなりました。。';
-      }
-    } else {
-      shouldNotify = false;
-      logger(date, 'no available court');
-    }
+    await scraper.init();
+    await scraper.run();
+    text = scraper.message;
+    shouldNotify = !stateSaver.isSameState(scraper.availableDate);
+    stateSaver.saveState(scraper.availableDate);
   } catch (error) {
     logger(error);
-    text = '処理に失敗しました。。';
+    text = 'Process Failed. Check log for detail';
   } finally {
     if (shouldNotify) {
-      logger('=== notify slack message ===');
-      logger(date);
-      logger(text);
-      await notifySlack(url, text);
+      await notifySlack(text);
+    } else {
+      logger('Nothing to notify.');
     }
   }
 })();
